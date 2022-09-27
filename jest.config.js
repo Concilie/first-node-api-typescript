f280b066b675f48f3e5fcf6b2633c4f4118ca698
winner <winnerconcilie@gmail.com>
2022-09-27 10:06:20 +0200

    add semi in eslint and node environnement + first set jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ["**/**/*.test.ts"], //tell jest where to find the test
+  verbose: true,
+  forceExit: true,
+  //clearMocks: true
 };
